f2d2cc0099220e8031a8a028a15adb0a5cc37a3b
Leonardo Veque <leonardo5veque@gmail.com>
2023-11-10 15:16:57 -0300

    Autenticação feita

--- app.js
+++ app.js
@@ -8,49 +8,112 @@ const cookieParser = require("cookie-parser");
 // Inicializar o express
 const app = express();
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 //Configurar os cookies
 app.use(cookieParser());
 
+app.use(express.urlencoded({ extended: true }));
+
 //Configurar as sessões
 app.use(
-    session({
-        secret: "minhaChave", //Chave secreta para assinar o cookies da sessão
-        resave: false, //Evita regravar sessões sem alterações
-        saveUninitialized: true, //Salva sessões não inicalizadas
-    })
+  session({
+    secret: "minhaChave", //Chave secreta para assinar o cookies da sessão
+    resave: false, //Evita regravar sessões sem alterações
+    saveUninitialized: true, //Salva sessões não inicalizadas
+  })
 );
+//Importação do env
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./.env",
+});
+
+//Importação do mysql
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DARTABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+//Importação do path
+const path = require("path", "../public");
+const publicDirectory = path.join(__dirname);
+app.use(express.static(publicDirectory));
+console.log(__dirname);
 
 const produtos = [
-    {
-        id: 1,
-        nome: "Arroz",
-        preco: 25,
-    },
-    {
-        id: 2,
-        nome: "Feijão",
-        preco: 28,
-    },
-    {
-        id: 3,
-        nome: "Bife",
-        preco: 40,
-    },
+  {
+    id: 1,
+    nome: "Arroz",
+    preco: 25,
+  },
+  {
+    id: 2,
+    nome: "Feijão",
+    preco: 28,
+  },
+  {
+    id: 3,
+    nome: "Bife",
+    preco: 40,
+  },
 ];
+
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Conecção feita");
+  }
+});
+
+app.set("view engine", "hbs");
+
+//Rota inicial
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//Auth
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  const login = req.body.login;
+  //Encriptando a senha
+  const senha = req.body.senha;
+
+  db.query("SELECT * FROM login WHERE login = ? ", [login], (err, results) => {
+    const result = results[0];
+
+    bcrypt.compare(senha, result.senha).then(function (match) {
+      if (match) {
+        res.redirect("/produtos");
+      } else {
+        res.redirect("/");
+        console.log("Senha incorreta");
+      }
+    });
+  });
+});
 
 // Rota inical para exibir produtos
 app.get("/produtos", (req, res) => {
-    res.send(`
+  res.send(`
         <h1>Lista de produtos</h1>
         <ul>
             ${produtos
-                .map(
-                    (produto) =>
-                        `<li>
+              .map(
+                (produto) =>
+                  `<li>
                     ${produto.nome} - ${produto.preco}<a href="/adicionar/${produto.id}">Adicionar ao carrinho</a>
                     </li>`
-                )
-                .join("")}
+              )
+              .join("")}
 
         </ul>
         <a href = "/carrinho">Ver Carrinho</a>
@@ -59,34 +122,34 @@ app.get("/produtos", (req, res) => {
 
 //Rota de Adicionar
 app.get("/adicionar/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const produto = produtos.find((p) => p.id === id);
+  const id = parseInt(req.params.id);
+  const produto = produtos.find((p) => p.id === id);
 
-    if (produto) {
-        if (!req.session.carrinho) {
-            req.session.carrinho = [];
-        }
-        req.session.carrinho.push(produto);
+  if (produto) {
+    if (!req.session.carrinho) {
+      req.session.carrinho = [];
     }
-    res.redirect("/produtos");
+    req.session.carrinho.push(produto);
+  }
+  res.redirect("/produtos");
 });
 
 //Carrinho rota
 app.get("/carrinho", (req, res) => {
-    const carrinho = req.session.carrinho || [];
-    const total = carrinho.reduce((acc, produto) => acc + produto.preco, 0);
+  const carrinho = req.session.carrinho || [];
+  const total = carrinho.reduce((acc, produto) => acc + produto.preco, 0);
 
-    res.send(`
+  res.send(`
         <h1>Carrinho de compras</h1>
         <ul>
             ${carrinho
-                .map(
-                    (produto) =>
-                        `<li>
+              .map(
+                (produto) =>
+                  `<li>
                     ${produto.nome} - ${produto.preco}
                         </li>`
-                )
-                .join("")}
+              )
+              .join("")}
 
         </ul>
         <p>Total: ${total}</p>
@@ -94,6 +157,16 @@ app.get("/carrinho", (req, res) => {
     `);
 });
 
+async function hashPassword(senha) {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(senha, salt);
+    return hash;
+  } catch (error) {
+    throw error;
+  }
+}
+
 app.listen(3000, () => {
-    console.log("Aplicação rodando");
+  console.log("Aplicação rodando");
 });
